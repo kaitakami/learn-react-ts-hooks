890671c8b38f45b05daab7846ea0d8e46db8e539
Kai Takami <kaitakami@protonmail.com>
2023-01-13 20:44:51 +0900

    Move interface to HookData in /types

--- src/components/ListEl.tsx
+++ src/components/ListEl.tsx
@@ -1,14 +1,9 @@
 import { Link } from "react-router-dom";
+import { HookData } from "../types";
 
-interface Props {
-  title: string;
-  description: string;
-  slug: string;
-}
-
-const ListEl = ({ title, description, slug }: Props) => {
+const ListEl = ({ title, description, slug }: HookData) => {
   return (
-    <li className="p-0 hover:bg-gray-100 border-slate-700 rounded-md border-2">
+    <li className="p-0 hover:bg-gray-100 border-slate-500 hover:border-slate-400 transition-all rounded-md border-2">
       <Link to={slug}>
         <div className="p-3 text-gray-600 hover:text-gray-500 hover:decoration-solid text-left">
           <h3 className="text-xl font-bold pl-4">{title}</h3>
