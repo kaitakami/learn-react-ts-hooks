0898344d0d12bee45739efc03d5a541be401c3df
Kai Takami <kaitakami@protonmail.com>
2023-01-15 14:41:28 +0900

    Create HooksData to render available classes

--- src/data/hooksData.ts
+++ src/data/hooksData.ts
@@ -10,5 +10,35 @@ export const hooksData: HookData[] = [
         title: "useEffect",
         description: "useEffect is a Hook in React that allows you to synchronize a component with an external system.",
         slug: "/useEffect"
+    },
+    {
+        title: "useContext",
+        description: "Learn useContext by creating an interactive Dark Mode button.",
+        slug: "/useContext"
+    },
+    {
+        title: "useReducer",
+        description: "Learn useReducer by creating an interactive to do list app.",
+        slug: "/useReducer"
+    },
+    {
+        title: "useRef",
+        description: "useRef allows you to create a reference to a DOM element or a component instance and it's great for accessing elements properties and methods.",
+        slug: "/useRef"
+    },
+    {
+        title: "Memoization",
+        description: "Before you move on, make sure you know what memoization is and how it helps make your apps run faster.",
+        slug: "/memoization"
+    },
+    {
+        title: "useMemo",
+        description: "Learn what useMemo is and how it helps make your apps run faster by only recalculating values when they are needed.",
+        slug: "/useMemo"
+    },
+    {
+        title: "useCallback",
+        description: "useCallback allows you to optimize your component's performance by only re-creating a callback function when its dependencies change.",
+        slug: "/useCallback"
     }
 ]
