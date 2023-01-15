9374e82c2d31dd1bdab1a6141a3fb4d9bfd0d579
Kai Takami <kaitakami@protonmail.com>
2023-01-15 20:52:04 +0900

    Add pages components and Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
 import { Routes, Route } from "react-router-dom";
-import UseState from "./pages/UseState";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import UseState from "./pages/UseState";
+import UseEffect from "./pages/UseEffect";
+import UseContext from "./pages/UseContext";
+import UseReducer from "./pages/UseReducer";
+import UseRef from "./pages/UseRef";
+import Memoization from "./pages/Memoization";
+import UseMemo from "./pages/UseMemo";
+import UseCallback from "./pages/UseCallback";
+
 
 function App() {
   return (
     <Routes>
-      <Route index path="/" element={<Home />} />
+      <Route index element={<Home />} />
+      <Route path="/about" element={<About />} />
       <Route path="/useState" element={<UseState />} />
+      <Route path="/useEffect" element={<UseEffect />} />
+      <Route path="/useContext" element={<UseContext />} />
+      <Route path="/useReducer" element={<UseReducer />} />
+      <Route path="/useRef" element={<UseRef />} />
+      <Route path="/memoization" element={<Memoization />} />
+      <Route path="/useMemo" element={<UseMemo />} />
+      <Route path="/useCallback" element={<UseCallback />} />
     </Routes>
   );
 }
