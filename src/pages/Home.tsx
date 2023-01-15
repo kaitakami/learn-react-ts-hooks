54b78889410a949983b122eb037940701dd6c70c
Kai Takami <kaitakami@protonmail.com>
2023-01-15 20:52:21 +0900

    Add About page in home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,13 @@
 import List from "../components/List";
 import '../App.css'
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div className="App">
+      <nav className="flex justify-end mr-8 mb-4">
+        <Link to="/about" className="font-bold text-slate-400">About</Link>
+      </nav>
       <h1>Learn react Hooks ⚛️</h1>
       <p>
         Here you can learn hooks in a practical way and easy to understand
@@ -12,6 +16,9 @@ const Home = () => {
       <div className="card">
         <List />
       </div>
+      <footer>
+        <p className="font-bold text-sm text-gray-500">©️2023 Kai Takami</p>
+      </footer>
     </div>
   );
 };
